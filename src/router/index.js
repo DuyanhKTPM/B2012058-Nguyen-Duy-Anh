5060ea991e132d2ee6dbe29983a5525c43ba21a1
DuyanhKTPM <anhb2012058@student.ctu.edu.vn>
2023-04-07 15:58:55 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,14 @@ const routes = [
     name: "notfound",
     component: NotFound,
   },
+  {
+  path: "/:pathMatch(.*)*",
+  name: "notfound",
+  component: () => import("@/views/NotFound.vue"),
+  },
 ];
+
+  
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
